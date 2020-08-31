459be99fdd0d93aec8d5cada136a7ad477efe895
DesaiSwara <68277364+DesaiSwara@users.noreply.github.com>
2020-08-31 17:48:34 +0530

    Add files via upload

--- Block.js
+++ Block.js
@@ -1,28 +1,32 @@
-class Polygon {
-    constructor(x, y, width, height) {
+class Block{
+  constructor(x,y,width,height){
       var options = {
-          'restitution':0.8,
-          'friction':1.0,
-          'density':1.0
-      }
-      this.body = Bodies.rectangle(x, y, width, height, options);
-      this.width = width;
-      this.height = height;
-      
-      World.add(world, this.body);
-    }
-    display(){
-      var pos =this.body.position;
-      var angle = this.body.angle;
-      push();
-      translate(pos.x, pos.y);
-      rotate(angle);
-      rectMode(CENTER);
-      strokeWeight(4);
-      stroke("green");
-      fill(255);
-      rect(0, 0, this.width, this.height);
-      pop();
-    }
-  };
+          isStatic: false,
+          restitution : 0.8 ,
+          density : 0.03,
+          friction : 0.3
+          
+      } 
+  this.body = Bodies.rectangle(x,y,width,height,options);
+  this.width = width;
+  this.height = height;
+  World.add(world,this.body)
+  
+ 
+ 
+  }
+ display(){
+ var pos = this.body.position;
+ var angle = this.body.angle;
+ push();
+ translate(pos.x,pos.y);
+ rotate(angle);
+ 
+ rectMode(CENTER);
+ rect(0,0,this.width,this.height);
+ pop();
+ 
+  }
+
+};
   
--- sketch.js
+++ sketch.js
@@ -4,32 +4,42 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var  stage1, stage2;
-var block1,block2,block3,block3,block4,block5,block6,block7,block8,block9,block10,block11,block12,block13,block14,block15,block16;
+var block1,block2,block3,block3,block4,block5,block6,block7,block8,block9,block10,block11,block12,block13,block14,block15,block16,block17,block18;
+var polygon,slingShot;
 
 function setup() {
-  var canvas=createCanvas(800,400);
+  var canvas=createCanvas(1600,800);
   engine = Engine.create();
   world = engine.world;
 
-  stage1=new Stage (200,200,50,50);
-  stage2=new Stage (400,400,50,50);
+  stage1=new Stage (1000,335,250,20);
+  stage2=new Stage (700,585,250,20);
 
-  block1=new Block (200,350,20,10);
-  block2=new Block (200,350,20,10);
-  block3=new Block (200,350,20,10);
-  block4=new Block (200,350,20,10);
-  block5=new Block (200,350,20,10);
-  block6=new Block (200,350,20,10);
-  block7=new Block (200,350,20,10);
-  block8=new Block (200,350,20,10);
-  block9=new Block (200,350,20,10);
-  block10=new Block (200,350,20,10);
-  block11=new Block (200,350,20,10);
-  block12=new Block (200,350,20,10);
-  block13=new Block (200,350,20,10);
-  block14=new Block (200,350,20,10);
-  block15=new Block (200,350,20,10);
-  block16=new Block (200,350,20,10);
+  slingShot=new SlingShot (polygon.body,{x:200,y:200})
+
+  block1=new Block (1000,200,40,50);
+  block2=new Block (1000,250,40,50);
+  block3=new Block (1000,300,40,50);
+  block4=new Block (960,250,40,50);
+  block5=new Block (1040,250,40,50);
+  block6=new Block (960,300,40,50);
+  block7=new Block (1040,300,40,50);
+  block8=new Block (1080,300,40,50);
+  block9=new Block (920,300,40,50);
+
+  block10=new Block (700,500,40,50);
+  block11=new Block (700,450,40,50);
+  block12=new Block (740,500,40,50);
+  block13=new Block (660,500,40,50);
+  block14=new Block (700,550,40,50);
+  block15=new Block (660,550,40,50);
+  block16=new Block (740,550,40,50);
+  block17=new Block (620,550,40,50);
+  block18=new Block (780,550,40,50);
+
+  polygon=new Polygon (200,200,50,50);
+
+  
 }
 
 function draw() {
@@ -37,6 +47,10 @@ function draw() {
 
   stage1.display();
   stage2.display();
+
+  polygon.display();
+
+  slingShot.display();
 
   block1.display();
   block2.display();
@@ -55,5 +69,16 @@ function draw() {
   block14.display();
   block15.display();
   block16.display();
+  block17.display();
+  block18.display();
   drawSprites();
 }
+
+function mouseDragged(){
+  Matter.Body.setPosition(polygon.body,{x:mouseX,y:mouseY});
+ 
+ }
+ function mouseReleased(){
+     slingShot.fly();
+ 
+ }
